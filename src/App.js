85879745b197704206b3bd537c57fa1fbd2e8235
matin-vilan <matinvilan1994@gmail.com>
2022-03-27 20:53:15 +0430

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from "react";
 import { isMobile } from "react-device-detect";
 import LiteCircular from "./components/global/Spinner/LiteCircular.jsx";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const AppComponent = isMobile?lazy(() => import("./components/desktop/Desktop.js")):lazy(() => import("./components/desktop/Desktop.js"));
 
