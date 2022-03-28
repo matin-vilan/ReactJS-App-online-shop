751064febb20abbdbab3238e1a26a06a2f9be83e
matin-vilan <matinvilan1994@gmail.com>
2022-03-28 19:19:08 +0430

    bootstrap imported

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store/store/store";
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./layouts/Desktop/assets/Desktop.css"
 
 
 
