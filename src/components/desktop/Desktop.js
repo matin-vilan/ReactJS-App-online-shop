8db05b0f8c360069a8e5fd25acde51fca4ddcba4
matin-vilan <matinvilan1994@gmail.com>
2022-03-28 19:52:14 +0430

    styles updated

--- src/components/desktop/Desktop.js
+++ src/components/desktop/Desktop.js
@@ -3,7 +3,7 @@ import Home from '../../layouts/Desktop/Home/Home'
 
 function Desktop() {
   return (
-    <div style={{direction:"rtl" , padding:".5rem"}}>
+    <div style={{direction:"rtl"}}>
       <Home />
     </div>
   )
