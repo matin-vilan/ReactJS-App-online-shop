31635504e3340ecee983ec3f61e443df8426c762
matin-vilan <matinvilan1994@gmail.com>
2022-03-28 19:53:07 +0430

    styles updated

--- src/components/desktop/Header/TopMenu/TopMenu.jsx
+++ src/components/desktop/Header/TopMenu/TopMenu.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import TopMenuNav from "./TopMenuNav/TopMenuNav";
 import TopMenuSocialMedia from "./TopMenuSocialMedia/TopMenuSocialMedia";
+import "./TopMenu.css";
 
 function TopMenu() {
   return (
     <div>
       <div className="row">
-        <div className="col-md-3 d-flex align-items-center">
-          <div>
-            <p>فروشگاه اینترنتی وودمارک</p>
+        <div className="col-md-2 d-flex align-items-center position-relative">
+          <div className="topMenu-title">
+            <p>فروشگاه اینترنتی وودمارت</p>
           </div>
+          <span className="verticalMenuDivider"></span>
         </div>
 
-        <div className="col-md-9">
+        <div className="col-md-10">
           <div className="row d-flex justify-content-between">
             <div className="TopMenuNav col-8">
               <TopMenuNav />
