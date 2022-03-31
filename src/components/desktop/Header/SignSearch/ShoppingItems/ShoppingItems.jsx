11e8c9d57ff04a1edb7a7965bb8c6bfccbb95a50
matin-vilan <matinvilan1994@gmail.com>
2022-03-31 17:44:24 +0430

    sign up menu created and updated

--- src/components/desktop/Header/SignSearch/ShoppingItems/ShoppingItems.jsx
+++ src/components/desktop/Header/SignSearch/ShoppingItems/ShoppingItems.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import "./ShoppingItems.css"
+import React from "react";
+import "./ShoppingItems.css";
 
 const ShoppingItems = (props) => {
-    return (
-        <div>
-            <div className='icon'>
-                {props.icon}
-            </div>
-            <div className='text'>
-                {props.text}
-            </div>
-            
-        </div>
-    );
+  return (
+    <div className="ms-2">
+      <div className="d-flex my-3 mx-2">
+        <div className="icon ms-2">{props.icon}</div>
+        <div className="text">{props.text}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingItems;
--- src/components/desktop/Header/SignSearch/SignSearch.jsx
+++ src/components/desktop/Header/SignSearch/SignSearch.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import VerticalDivider from "../../../global/Divider/VerticalDivider/VerticalDivider";
 import ShoppingItems from "./ShoppingItems/ShoppingItems";
 
 function SignSearch() {
   return (
     <div>
-      <div className="row p-0 m-0 d-flex">
-        <ShoppingItems icon={<i></i>} />
+      <div className="signUp p-0 m-0 d-flex">
+          <ShoppingItems icon={<i class="bi bi-bag"></i>} text={"0 تومان"} />
+
+        <div class="vr" style={{alignSelf:"center" , minHeight:"1.5em"}} ></div>
+
+          <ShoppingItems icon={<i class="bi bi-heart"></i>} text={"علاقه مندی"} />
+
+        <div class="vr" style={{alignSelf:"center" , minHeight:"1.5em"}} ></div>
+
+          <ShoppingItems icon={<i class="bi bi-arrow-left-right"></i>} text={"مقایسه"} />
+
+        <div class="vr" style={{alignSelf:"center" , minHeight:"1.5em"}} ></div>
+
+          <ShoppingItems icon={<i class="bi bi-person"></i>} text={"ورود / ثبت نام"} />
+      </div>
+
+      <div className="search">
+        <div className="">
+          
+        </div>
       </div>
     </div>
   );
