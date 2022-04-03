debba44dc93ba5583853708e00d39566fa86926c
matin-vilan <matinvilan1994@gmail.com>
2022-04-03 12:51:47 +0430

    menu list and menu item modified

--- src/components/desktop/Header/BottomMenu/MenuList/MenuItem/MenuItem.jsx
+++ src/components/desktop/Header/BottomMenu/MenuList/MenuItem/MenuItem.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Dropdown } from "react-bootstrap";
+import styles from "./MenuItem.module.css";
 
 const MenuItem = ({ text }) => {
+  const [active, setActive] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const activeHandler = () => {
+    setActive(!active);
+    setShow(!show);
+  };
+
   return (
     <>
-      <Dropdown className="ms-2">
-        <Dropdown.Toggle variant="none" id="dropdown-basic">
+      <Dropdown
+        className="ms-2"
+        show={show}
+        onMouseEnter={activeHandler}
+        onMouseLeave={activeHandler}
+      >
+        <Dropdown.Toggle
+          variant={"none"}
+          className={active ? "text-danger" : ""}
+          id="dropdown-basic"
+        >
           {text}
         </Dropdown.Toggle>
 
-        <Dropdown.Menu>
+        <Dropdown.Menu className={styles.dropDownMenuItem}>
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
--- src/components/desktop/Header/BottomMenu/MenuList/MenuList.jsx
+++ src/components/desktop/Header/BottomMenu/MenuList/MenuList.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import MenuItem from './MenuItem/MenuItem';
+import React from "react";
+import MenuItem from "./MenuItem/MenuItem";
 import styles from "./MenuList.module.css";
 
 const MenuList = () => {
-    return (
-        <div className='d-flex justify-content-start'>
-            <MenuItem text={"صفحه نخست"} />
-            <MenuItem text={"فروشگاه"} />
-        </div>
-    );
+
+  const data = [
+    {
+      text: "صفحه نخست",
+    },
+    {
+      text: "فروشگاه",
+    },
+  ];
+
+  return (
+    <div className="d-flex justify-content-start">
+      {data.map((item, index) => {
+        return (
+          <MenuItem
+            key={index}
+            text={item.text}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default MenuList;
