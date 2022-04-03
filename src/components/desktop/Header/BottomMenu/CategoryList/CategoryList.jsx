a5c5c94bd7df801774fe4d0675d6cae8c3bf4228
matin-vilan <matinvilan1994@gmail.com>
2022-04-03 19:50:06 +0430

    category list updated

--- src/components/desktop/Header/BottomMenu/CategoryList/CategoryList.jsx
+++ src/components/desktop/Header/BottomMenu/CategoryList/CategoryList.jsx
@@ -1,51 +1,35 @@
-import React  from "react";
-import {useState} from "react";
+import React from "react";
+import { useState } from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import styles from "./CategoryList.module.css";
 import * as Icon from "react-bootstrap-icons";
 
 const CategoryList = () => {
+  const [hovered, setHovered] = useState(false);
 
-  const [hovered , setHovered] = useState(false);
-
-
-  const handleMenuHoverIn = ()=>{
+  const handleMenuHoverIn = () => {
     setHovered(!hovered);
-  }
+  };
 
-  const handleMenuHoverOut = ()=>{
+  const handleMenuHoverOut = () => {
     setHovered(false);
-  }
-
+  };
 
   const dropDownMenu = hovered ? "show" : "";
-  console.log(dropDownMenu);
 
   return (
     <>
-      <Navbar expand="lg" id="categoryList" onMouseEnter={handleMenuHoverIn} onMouseLeave={handleMenuHoverOut}>
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Icon.List style={{ fontSize: "1.5rem" }} />
-            <Nav className="ms-auto">
-              <NavDropdown title="لیست دسته بندی ها" id="basic-nav-dropdown" show={dropDownMenu}>
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div className="d-flex align-items-center justify-content-between w-100">
+        <div className="dropdownMenu d-flex justify-content-start align-items-center">
+          <div className={styles.icon}>
+            <Icon.List />
+          </div>
+          <div className={styles.listTitle}>دسته بندی کالاها</div>
+        </div>
+        <div className={styles.dropToggleIcon}>
+          <Icon.ChevronDown />
+        </div>
+      </div>
     </>
   );
 };
