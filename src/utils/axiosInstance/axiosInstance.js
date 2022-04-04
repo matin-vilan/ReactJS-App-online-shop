1850a9e66026072aae8873f493462c65d9b531a1
matin-vilan <matinvilan1994@gmail.com>
2022-04-04 14:16:19 +0430

    update base url

--- src/utils/axiosInstance/axiosInstance.js
+++ src/utils/axiosInstance/axiosInstance.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
- const axiosInstanceApi = axios.create({
-    baseURL: "https://valiasrcs.com/fa/wp-json/wp/v2/"
-})
+const axiosInstanceApi = () => {
+    return axios.create({
+        baseURL: "https://valiasrcs.com/fa/wp-json/wp/v2/"
+    })
+}
 
-const axiosInstanceApiMenu = axios.create({
-    baseURL: "https://valiasrcs.com/vApp/wp-json/wp-api-menus/v2/menus/"
-})
-
+export const axiosInstanceApiMenu = ()=>{
+    return axios.create({
+        baseURL:"https://valiasrcs.com/vApp/wp-json/wp-api-menus/v2/menus"
+    })
+}
 
 export default axiosInstanceApi;
+
+
+
