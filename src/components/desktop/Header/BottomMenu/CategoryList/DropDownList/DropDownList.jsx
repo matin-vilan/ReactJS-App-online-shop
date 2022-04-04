369e08db621d47223e8f1ff8131ecb0727c5feaa
matin-vilan <matinvilan1994@gmail.com>
2022-04-04 12:20:28 +0430

    category list completed

--- src/components/desktop/Header/BottomMenu/CategoryList/DropDownList/DropDownList.jsx
+++ src/components/desktop/Header/BottomMenu/CategoryList/DropDownList/DropDownList.jsx
@@ -3,7 +3,7 @@ import Divider from '../../../../../global/Divider/Divider';
 import DropDownItem from './DropDownItem/DropDownItem';
 import styles from "./DropDownList.module.css";
 
-const DropDownList = ({show}) => {
+const DropDownList = ({ show }) => {
 
     const data = [
 
@@ -28,7 +28,7 @@ const DropDownList = ({show}) => {
     ]
 
     return (
-        <div className={show ? "d-block":"d-none" }>
+        <div className={show ? styles.dropDownShow : styles.dropDownHide}>
             <ul className={styles.dropDownUl}>
                 {data.map((item, index) => {
                     return <>
