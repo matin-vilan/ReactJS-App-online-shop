e792864b05ff8f6eac459fe90b85cf0ac9328f3c
matin-vilan <matinvilan1994@gmail.com>
2022-04-04 12:20:06 +0430

    menu api created

--- src/utils/axiosInstance/axiosInstance.js
+++ src/utils/axiosInstance/axiosInstance.js
@@ -4,5 +4,9 @@ import axios from "axios";
     baseURL: "https://valiasrcs.com/fa/wp-json/wp/v2/"
 })
 
+const axiosInstanceApiMenu = axios.create({
+    baseURL: "https://valiasrcs.com/vApp/wp-json/wp-api-menus/v2/menus/"
+})
+
 
 export default axiosInstanceApi;
