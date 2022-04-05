35b63f677c64ba67447f937586d6900549f859d3
matin-vilan <matinvilan1994@gmail.com>
2022-04-05 18:44:39 +0430

    top main layout created

--- src/components/desktop/Main/Main.jsx
+++ src/components/desktop/Main/Main.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import TopMainSquareImage from './TopMainImages/TopMainSquareImage/TopMainSquareImage'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div className={"mainLayout"}>
+      <div className='row p-0 m-0 my-5'>
+
+        <div className='row p-0 m-0 d-flex col-12 mb-4'>
+          <div className='col-4'>
+            <TopMainSquareImage src={"/assets/images/mainImages/1.jpg"} />
+          </div>
+          <div className='col-8'>
+            col-8
+          </div>
+        </div>
+
+        <div className='row p-0 m-0 d-flex col-12'>
+          <div className='col-4'>
+          <TopMainSquareImage src={"assets/images/mainImages/2.jpg"} />
+          </div>
+          <div className='col-4'>
+          <TopMainSquareImage src={"assets/images/mainImages/3.jpg"} />
+          </div>
+          <div className='col-4'>
+          <TopMainSquareImage src={"assets/images/mainImages/4.jpg"} />
+          </div>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
